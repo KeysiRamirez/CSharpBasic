a2894f69c87de7a09b1c8cf606f779ab62aa0d7f
KEYSI MICHELL RAMIREZ MATEO <k.ramirezm@unapec.edu.do>
2024-09-09 21:47:23 -0400

    Programs updated

--- CSharpBasic/AvgGrades/AvgGrades.cs
+++ CSharpBasic/AvgGrades/AvgGrades.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace CSharpBasic.AvgGrades
 {
-    internal class AvgGrades
+    /// <summary>
+    /// Clase para leer tres calificaciones y calcular
+    /// el promedio. Si el promedio es igual o superior a siete, 
+    /// imprimir un mensaje indicando que el alumno ha aprobado.
+    /// </summary>
+    public class AvgGrades
     {
+        public void AvgGradesCalculator()
+        {
+            // Declaring the variables
+            int sum_grades = 0, average_grade = 0;
+            string input = string.Empty;
+
+            // Request for the 3 grades
+            for (int i = 1; i <= 3; i++)
+            {
+                // To get the grades
+                Console.Write("Enter the grade: ");
+                input = Console.ReadLine();
+
+                // Validate if the string is null
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.Write($" Invalid input entered: {input}, you must enter a number");
+                    return;
+                }
+                // To validate if the number is not 0
+
+                else if (!int.TryParse(input, out sum_grades))
+                {
+                    Console.WriteLine($"The entered number is invalid {input}");
+                    return;
+                }
+
+                else
+                {
+                    // To convert the input values to an integer in order to get the average
+                    sum_grades += Convert.ToInt32(input);
+                    average_grade = sum_grades / 3;
+                }
+            }
+
+            // Calculate the average for the input grades
+
+
+            // Defining the condition according the average
+            string result = average_grade >= 7 ? $"Studient passed with: {average_grade}" : $"Studient failed with: {average_grade}";
+
+            // Output the condition
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
     }
 }
--- CSharpBasic/CalculateNumbers/CalculateNumbers.cs
+++ CSharpBasic/CalculateNumbers/CalculateNumbers.cs
@@ -6,7 +6,99 @@ using System.Threading.Tasks;
 
 namespace CSharpBasic.CalculateNumbers
 {
-    internal class CalculateNumbers
+    /// <summary>
+    /// Clase para calcular dos numeros intrducidos por el usuario si el primer número es más grande, calcule y muestre su suma y resta. 
+    /// De lo contrario, calculará y mostrará su producto y cociente.
+    /// </summary>
+    public class CalculateNumbers
     {
+        public void Calculate()
+        {
+            // Declaring the variables
+            int num1 = 0, num2 = 0, substraction = 0, addition = 0;
+            decimal division = 0, multiplication = 0;
+            string FirstNumber = string.Empty, SecondNumber = string.Empty;
+
+            try
+            {
+                // To get the first number
+                Console.Write("Input the first number: ");
+                FirstNumber = Console.ReadLine();
+
+                // To validate if the user didn't enter an empty string for the first one
+                if (string.IsNullOrEmpty(FirstNumber))
+                {
+                    Console.WriteLine("You must enter a number, the field cannot be null");
+                    return;
+                }
+
+                // To validate if the user entered a valid number first one
+                if (!int.TryParse(FirstNumber, out num1))
+                {
+                    Console.Write($"Invalid number input: {FirstNumber}");
+                    return;
+                }
+
+                // To get the second number
+                Console.Write("Input the second number: ");
+                SecondNumber = Console.ReadLine();
+
+                // To validate if the user didn't enter an empty string for the second one
+                if (string.IsNullOrEmpty(SecondNumber))
+                {
+                    Console.WriteLine("You must enter a number, the field cannot be null");
+                    return;
+                }
+
+                // To validate if the user entered a valid number first one 
+                if (!int.TryParse(SecondNumber, out num2))
+                {
+                    Console.Write($"Invalid number input: {SecondNumber}");
+                    return;
+                }
+
+                // In order to calculate
+                if (num1 > num2)
+                {
+                    // To add up the numbers
+                    addition = (num1 + num2);
+
+                    // To subtract up the numbers
+                    substraction = (num1 - num2);
+
+                    Console.WriteLine($"The sum result is: {addition}");
+                    Console.WriteLine($"The subtraction result is: {substraction}");
+
+                }
+
+                // To validate if the user didn't enter a 0 
+                else if (num1 == 0 || num2 == 0)
+                {
+                    Console.WriteLine("It's unable divide a number by 0");
+                }
+
+                else
+                {
+                    // To add up the numbers
+                    division = (decimal)num2 / (decimal)num1; // (decimal) is used to convert the integers to decimals
+
+                    // To subtract up the numbers
+                    multiplication = (num1 * num2);
+
+                    Console.WriteLine($"The division result is: {division:F2}");
+                    Console.WriteLine($"The multiplication result is: {multiplication}");
+
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Occured an error during the running: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+
     }
 }
